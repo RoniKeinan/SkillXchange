d36ad7e3d46362be7d4dbb886cc9e93c680ae5e0
Evyatarmk <evyatar.mt@gmail.com>
2025-06-22 16:48:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { SkillProvider } from './contexts/SkillsContext';
 import { CategoryProvider } from './contexts/CategoryContext';
 import { UserProvider } from './contexts/UserContext';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { RequestProvider } from './contexts/RequestContext';
 import { ChatProvider } from './contexts/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<Router>
-  <UserProvider>
-    <CategoryProvider>
-      <SkillProvider>
-        <ChatProvider>
-          <RequestProvider>
-            <App />
-          </RequestProvider>
-        </ChatProvider>
-      </SkillProvider>
-    </CategoryProvider>
-  </UserProvider>
-</Router>
+  <Router>
+    <UserProvider>
+      <CategoryProvider>
+        <SkillProvider>
+          <ChatProvider>
+            <RequestProvider>
+              <App />
+            </RequestProvider>
+          </ChatProvider>
+        </SkillProvider>
+      </CategoryProvider>
+    </UserProvider>
+  </Router>
 );
